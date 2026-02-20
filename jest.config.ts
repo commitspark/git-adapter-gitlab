9d4b8bdd4bc8bdb92c5d5485c5abcd1586780371
Markus Weiland <mw@2h10.de>
2026-02-20 20:53:36 +0100

    Add Jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+import type { Config } from 'jest'
+
+const jestConfig: Config = {
+  moduleFileExtensions: ['js', 'ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+}
+
+export default jestConfig
